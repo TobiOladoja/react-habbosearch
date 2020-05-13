a6e62dd62a43f60f73be05b140ce4d78a0bdaef6
TobiOladoja <tbfullstack@gmail.com>
2020-05-13 17:25:05 +0100

    API call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+  const [names, setNames] = useState([]);
+  const [descriptions, setDescriptions] = useState([]);
+  const [links, setLinks] = useState([]);
+
+  async function makeSearch() {
+    const url = `https://en.wikipedia.org/w/api.phpaction=opensearch&search=${search}&format=json&origin=*`;
+    const response = await fetch(url);
+    const jsonRes = await response.json();
+
+    setNames(jsonRes[1]);
+    setDescriptions(jsonRes[2]);
+    setLinks(jsonRes[3]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='box-wrapper'>
+      <div className='box'>
+        <h3>React Wikipedia Viewer</h3>
+        <input value={search} onChange={(e) => setSearch(e.target.value)} />
+        <button onClick={makeSearch}>Search</button>
+
+        {names.map((name) => (
+          <div> {name} </div>
+        ))}
+      </div>
     </div>
   );
 }
